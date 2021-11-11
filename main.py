393d9f1dc3d657cc8a469b208b4fa2fd1f50032f
Nik Pirc <nik@oaa.si>
2021-11-11 18:17:32 +0100

    added cookies and post requests

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    weather_info = "Danes je zelo lep dan"
+    current_time = datetime.datetime.now()
+    time = current_time.strftime('%H:%M:%S')
+    return render_template("index.html", weather_info=weather_info, time=time)
 
-@app.route("/about")
+@app.route("/about", methods=["GET", "POST"])
 def about():
-    return render_template("about.html")
+    if request.method == "GET":
+        name = request.cookies.get("name")
+        return render_template("about.html", name=name)
+    elif request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        print(name)
+        print(email)
+        print(message)
+
+        response = make_response(render_template("success.html"))
+        response.set_cookie("name", name)
+        return response
+
 
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
 
 if __name__ == "__main__":
-    app.run(use_reloader=True)
+    app.run(use_reloader=True, debug=True)
